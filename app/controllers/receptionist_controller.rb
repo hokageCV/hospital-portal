131c2e615c8b5d6287fc282300b3e34e1c21f410
hokageCV <chaitanyavaru@gmail.com>
2023-08-17 20:13:59 +0530

    feat: input new record data

--- app/controllers/receptionist_controller.rb
+++ app/controllers/receptionist_controller.rb
@@ -2,4 +2,19 @@ class ReceptionistController < ApplicationController
     def new
         @new_patient_record = PatientRecord.new
     end
+
+    def create
+        @new_patient_record = PatientRecord.new(patient_params)
+        if @new_patient_record.save
+          redirect_to receptionist_path, notice: "Patient record created successfully."
+        else
+          render :new
+        end
+      end
+    
+      private
+    
+      def patient_params
+        params.require(:patient_record).permit(:name, :age)
+      end
 end
