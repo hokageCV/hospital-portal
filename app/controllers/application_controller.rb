62e340ca40466bf19094613820d678bf1c560bf4
hokageCV <chaitanyavaru@gmail.com>
2023-08-19 20:20:02 +0530

    fix: login keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
+    # puts "🔥 Resource object yeh hai: #{resource.inspect}"
     if resource.receptionist?
       receptionist_path
     elsif resource.doctor?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :timeoutable, :registerable, :recoverable, :rememberable, :validatable, :authentication_keys => [:email, :role]
+  devise :database_authenticatable, :timeoutable, :registerable, :recoverable, :rememberable, :validatable, :authentication_keys => [:email]
 
   enum role: { receptionist: "receptionist", doctor: "doctor" }
 end
